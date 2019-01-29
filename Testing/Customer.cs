0f25642def364f2509971b21983577b9050c5c7f
Kostas Stroggylos <kostas.stroggylos@peoplecert.onmicrosoft.com>
2019-01-29 19:56:54 +0200

    skeleton for mocking

---
+++ Testing/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace Testing
+{
+    public class Customer
+    {
+        public int Cid { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string City { get; set; }
+    }
+}
---
+++ Testing/DatabaseManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    public class DatabaseManager
+    {
+        private static string _connectionString = "Server=localhost;Database=bank;Trusted_Connection=True;";
+
+        public Customer FindCustomerById(int cid)
+        {
+            //TODO: using dapper, select the customer with the given id and return it
+            return null;
+        }
+
+
+    }
+}
