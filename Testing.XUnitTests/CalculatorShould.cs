cb6b77c149b3a3fc69b3b0bcc9dfa4319ec9755a
Kostas Stroggylos <kostas.stroggylos@peoplecert.onmicrosoft.com>
2019-02-04 21:20:13 +0200

    xunit

---
+++ Testing.XUnitTests/CalculatorShould.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Testing.XUnitTests
+{
+    public class CalculatorShould
+    {
+        [Theory]
+        [InlineData(0, 0, 0)]
+        [InlineData(1, -1, 0)]
+        [InlineData(0, 5, 5)]
+        [InlineData(0.5, 0.5, 1)]
+        public void AddNumbers(double x, double y, double expected)
+        {
+            var c = new Calculator();
+            Assert.Equal(expected, c.Add(x, y));
+        }
+
+        [Fact]
+        public void Moufa()
+        {
+            var c = new Calculator();
+            Assert.IsType<Calculator>(c);
+        }
+    }
+}
